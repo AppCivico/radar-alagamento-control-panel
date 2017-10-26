bac36a4d46f4d3dd02a5011ac4d373112f3170ee
Laís Tomaz <lais@eokoe.com>
2017-10-26 18:35:41 -0200

    Create header component

---
+++ src/javascripts/app/components/layout/header.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import userDefault from '../../../../images/user-default.png';
+
+class Header extends React.Component {
+	render() {
+		return (
+			<header className="main-header">
+				<a href="/" className="logo">
+					<span className="logo-mini">RdA</span>
+					<span className="logo-lg">Radar do <strong>Alagamento</strong></span>
+				</a>
+				<nav className="navbar navbar-static-top">{
+		        // eslint-disable-next-line jsx-a11y/href-no-hash
+		        }<a href="#" className="sidebar-toggle" data-toggle="push-menu" role="button">
+						<span className="sr-only">toggle</span>
+					</a>
+
+					<div className="navbar-custom-menu">
+						<ul className="nav navbar-nav">
+							<li className="dropdown user user-menu">{
+				        // eslint-disable-next-line jsx-a11y/href-no-hash
+				        }<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+									<img src={userDefault} className="user-image" alt="User Avatar" />
+									<span className="hidden-xs">User</span>
+								</a>
+								<ul className="dropdown-menu">
+									<li className="user-header">
+										<img src={userDefault} className="img-circle" alt="User Avatar" />
+										<p>
+											User
+											<small>E-mail</small>
+										</p>
+									</li>
+									<li className="user-footer">
+										<div className="pull-right">{
+							        // eslint-disable-next-line jsx-a11y/href-no-hash
+							        }<a href="#" className="btn btn-default btn-flat">Sair</a>
+										</div>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</header>
+		)
+	}
+}
+
+export default Header;
