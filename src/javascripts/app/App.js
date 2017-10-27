ea07fd97dbb8a6bc8bd56875a833a139a3902bd3
Laís Tomaz <lais@eokoe.com>
2017-10-27 12:23:23 -0200

    Add menu to App.js

--- src/javascripts/app/App.js
+++ src/javascripts/app/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 import Header from './components/layout/header';
+import Menu from './components/layout/menu';
 
 class App extends Component {
   render() {
     return (
-      <Header></Header>
+    	<div className="wrapper">
+	      <Header></Header>
+	      <Menu></Menu>
+	      <div className="content-wrapper">
+	      </div>
+      </div>
     );
   }
 }
