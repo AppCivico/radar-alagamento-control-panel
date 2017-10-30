015feeb83228165c8657a74aadb697befc28ea52
Laís Tomaz <lais@eokoe.com>
2017-10-30 13:50:14 -0200

    Use className instead of class

--- src/javascripts/app/components/layout/menu.js
+++ src/javascripts/app/components/layout/menu.js
@@ -3,38 +3,38 @@ import React from 'react';
 class Menu extends React.Component {
 	render() {
 		return (
-			<aside class="main-sidebar">
-				<section class="sidebar">
-					<ul class="sidebar-menu" data-widget="tree">
-						<li class="treeview">{
+			<aside className="main-sidebar">
+				<section className="sidebar">
+					<ul className="sidebar-menu" data-widget="tree">
+						<li className="treeview">{
 				        // eslint-disable-next-line jsx-a11y/href-no-hash
 				      }<a href="#">
-								<i class="fa fa-pie-chart"></i>
+								<i className="fa fa-pie-chart"></i>
 								<span>texto</span>
-								<span class="pull-right-container">
-									<i class="fa fa-angle-left pull-right"></i>
+								<span className="pull-right-container">
+									<i className="fa fa-angle-left pull-right"></i>
 								</span>
 							</a>
-							<ul class="treeview-menu">
-								<li><a href="/categories"><i class="fa fa-circle-o"></i>texto</a></li>
-								<li><a href="/results"><i class="fa fa-circle-o"></i>texto</a></li>
+							<ul className="treeview-menu">
+								<li><a href="/categories"><i className="fa fa-circle-o"></i>texto</a></li>
+								<li><a href="/results"><i className="fa fa-circle-o"></i>texto</a></li>
 							</ul>
 						</li>
-						<li class="treeview">
+						<li className="treeview">
 							<a href="/complaints/0">
-								<i class="fa fa-hand-o-right"></i>
+								<i className="fa fa-hand-o-right"></i>
 								<span>texto</span>
 							</a>
 						</li>
-						<li class="treeview">
+						<li className="treeview">
 							<a href="/complaints/1">
-								<i class="fa fa-hand-grab-o"></i>
+								<i className="fa fa-hand-grab-o"></i>
 								<span>texto</span>
 							</a>
 						</li>
-						<li class="treeview">
+						<li className="treeview">
 							<a href="/notifications">
-								<i class="fa fa-bell"></i>
+								<i className="fa fa-bell"></i>
 								<span>texto</span>
 							</a>
 						</li>
