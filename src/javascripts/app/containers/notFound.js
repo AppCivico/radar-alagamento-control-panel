d0ceb3d43a9e3ae50d9659f15e8f75a8c2794cd7
Laís Tomaz <laisotomaz@gmail.com>
2017-10-31 19:38:46 -0200

    Create notFound component

---
+++ src/javascripts/app/containers/notFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+	render() {
+		return (
+			<div className="col-xs-12">
+				<div className="box">
+					<div className="box-header">
+						<h3 className="box-title">Not found</h3>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default NotFound;
