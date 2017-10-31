e780a4fcd79695b874ec14072e6c945b06eded01
Laís Tomaz <laisotomaz@gmail.com>
2017-10-31 19:07:28 -0200

    Create notification component

---
+++ src/javascripts/app/component/notification.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Notification extends React.Component {
+
+	render() {
+		const notification = this.props.notification;
+
+		return (
+			<tr>
+        <td>{notification.id}</td>
+        <td>{notification.title}</td>
+        <td>{notification.date}</td>
+        <td><span className={`label label-${notification.level}`}>{notification.level}</span></td>
+        <td>{notification.description}</td>
+      </tr>
+		)
+	}
+}
+
+export default Notification;
