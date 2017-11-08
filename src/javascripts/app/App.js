3ce827a24b90a9202980d275c86b896bb588828b
Laís Tomaz <laisotomaz@gmail.com>
2017-11-08 13:57:33 -0200

    Move Router to make it the parent node

--- src/javascripts/app/App.js
+++ src/javascripts/app/App.js
@@ -13,22 +13,22 @@ import NotFound from './containers/notFound';
 class App extends Component {
 	render() {
 		return (
-			<div className="wrapper">
-				<Header></Header>
-				<Menu></Menu>
-				<div className="content-wrapper">
-					<div className="content">
-						<div className="row">
-							 <Router>
+			<Router>
+				<div className="wrapper">
+					<Header></Header>
+					<Menu></Menu>
+					<div className="content-wrapper">
+						<div className="content">
+							<div className="row">
 							 	<Switch>
 									<Route path="/notifications" component={Notifications}/>
 									<Route component={NotFound}/>
 								</Switch>
-							</Router>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
