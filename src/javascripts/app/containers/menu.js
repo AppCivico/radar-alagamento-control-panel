184ea55fcdd900fd9083006fa0a9a850761b9e8d
Laís Tomaz <laisotomaz@gmail.com>
2017-11-08 13:57:44 -0200

    Remove Router duplication

--- src/javascripts/app/containers/menu.js
+++ src/javascripts/app/containers/menu.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-	BrowserRouter as Router,
-	Link }
-	from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Menu extends React.Component {
 	render() {
@@ -10,14 +7,12 @@ class Menu extends React.Component {
 			<aside className="main-sidebar">
 				<section className="sidebar">
 					<ul className="sidebar-menu" data-widget="tree">
-						<Router>
 							<li className="treeview">
 								<Link to="/notifications">
 									<i className="fa fa-hand-o-right"></i>
 									<span>Notificações</span>
 								</Link>
 							</li>
-						</Router>
 					</ul>
 				</section>
 			</aside>
