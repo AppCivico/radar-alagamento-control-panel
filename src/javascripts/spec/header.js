79363363cf328f60200781d023b4073e54ebf8f7
Laís Tomaz <lais@eokoe.com>
2017-11-09 14:49:57 -0200

    Fix header tests

--- src/javascripts/spec/header.js
+++ src/javascripts/spec/header.js
@@ -1,26 +1,8 @@
-import React from 'react'
-import chaiEnzyme from 'chai-enzyme'
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow } from 'enzyme'
-import { configure } from 'enzyme';
-
-import Header from '../app/containers/header';
-
-configure({ adapter: new Adapter() });
-
-// HACK: chai-enzyme does not play nice with chai-jquery, so remove the
-// problem-causing assertions that collide with chai-jquery
-'visible hidden selected checked enabled disabled'.split(' ').forEach((selector) => {
-  Object.defineProperty(chai.Assertion.prototype, selector, { get: () => {} })
-})
-
-chai.use(chaiEnzyme())
-
-describe('Header component', () => {
-  context('<Header />', () => {
-    it('renders correctly', () => {
-      const component = shallow(<Header />)
-      expect(component.find('.sr-only')).to.have.text('toggle')
+describe('<Header />', () => {
+  context('e2e', () => {
+    it('toggle user', () => {
+    	cy.visit('');
+    	cy.get('ul li:first').click().should('have.class', 'open')
     });
   })
 })
