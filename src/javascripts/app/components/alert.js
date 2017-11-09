7b3d8ddff5b8787a02b1c689657dac66974f28eb
Laís Tomaz <lais@eokoe.com>
2017-11-09 16:57:53 -0200

    Create alert modal component

---
+++ src/javascripts/app/components/alert.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Alert extends React.Component {
+	render() {
+		return (
+			<div className="modal fade in" id="alert">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<button type="button" className="close" data-dismiss="modal" aria-label="Cancelar">
+								<span aria-hidden="true">×</span>
+							</button>
+							<h4 className="modal-title">Criar alerta</h4>
+						</div>
+						<div className="modal-body">
+							<div className="form-group">
+								<label>wefwef</label>
+							</div>
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-default pull-left" data-dismiss="modal">Cancelar</button>
+							<button type="button" className="btn btn-primary">Enviar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Alert;
