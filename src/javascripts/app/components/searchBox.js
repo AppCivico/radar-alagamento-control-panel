a09d81ec529d390a3871ebdbc37b2f560f4f0dbd
Laís Tomaz <lais@eokoe.com>
2017-11-09 16:44:48 -0200

    Create searchBox component

---
+++ src/javascripts/app/components/searchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchBox extends React.Component {
+	render() {
+		return (
+			<div className="input-group input-group-sm notifications-search">
+				<input type="text" name="table_search" className="form-control pull-right" placeholder="Search" />
+
+				<div className="input-group-btn">
+					<button type="submit" className="btn btn-default"><i className="fa fa-search"></i></button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default SearchBox;
