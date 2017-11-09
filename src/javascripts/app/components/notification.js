939515b16957f47d7a46ac1e569ada2640261b3c
Laís Tomaz <lais@eokoe.com>
2017-11-09 15:55:23 -0200

    Add notification call to action buttons

--- src/javascripts/app/components/notification.js
+++ src/javascripts/app/components/notification.js
@@ -12,6 +12,12 @@ class Notification extends React.Component {
 				<td>{notification.date}</td>
 				<td><span className={`label label-${notification.level}`}>{notification.level}</span></td>
 				<td>{notification.description}</td>
+				<td>
+					<div class="btn-group pull-right">
+            <button type="button" className="btn btn-danger">Criar notificação</button>
+            <button type="button" className="btn btn-info">Ignorar</button>
+          </div>
+				</td>
 			</tr>
 		)
 	}
