bc687b8ca5146a5ca108d4ae6330678e7ed97421
Laís Tomaz <lais@eokoe.com>
2017-11-09 15:55:02 -0200

    Enhance header tests

--- src/javascripts/spec/header.js
+++ src/javascripts/spec/header.js
@@ -2,7 +2,13 @@ describe('<Header />', () => {
   context('e2e', () => {
     it('toggle user', () => {
     	cy.visit('');
-    	cy.get('ul li:first').click().should('have.class', 'open')
+    	cy.get('.user-menu').click().should('have.class', 'open')
+    });
+
+    it('toggle sidebar', () => {
+    	cy.visit('');
+    	cy.get('.sidebar-toggle').click()
+    	cy.get('body').should('have.class', 'sidebar-collapse')
     });
   })
 })
