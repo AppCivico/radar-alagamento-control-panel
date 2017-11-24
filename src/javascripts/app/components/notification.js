12e5faa50cd91ddd29832b0dba4e08a19af494cd
Laís Tomaz <lais@eokoe.com>
2017-11-24 18:51:59 -0200

    Fetch notifications from api and format date

--- src/javascripts/app/components/notification.js
+++ src/javascripts/app/components/notification.js
@@ -1,34 +1,51 @@
-import React from 'react';
+import React from "react";
 
 class Notification extends React.Component {
-	constructor() {
-		super();
-		this.createNotification = this.createNotification.bind(this);
-	}
+  constructor() {
+    super();
+    this.createNotification = this.createNotification.bind(this);
+  }
 
-	createNotification() {
-		this.props.toggleModal(true);
-	}
+  createNotification() {
+    this.props.toggleModal(true);
+  }
 
-	render() {
-		const notification = this.props.notification;
+  formatDate(date) {
+    return date
+      .split(" ")[0]
+      .split("-")
+      .reverse()
+      .join("/");
+  }
 
-		return (
-			<tr className="notification">
-				<td>{notification.id}</td>
-				<td>{notification.title}</td>
-				<td>{notification.date}</td>
-				<td><span className={`label label-${notification.level}`}>{notification.level}</span></td>
-				<td>{notification.description}</td>
-				<td>
-					<div className="btn-group pull-right">
-            <button type="button" className="btn btn-danger" onClick={() => this.createNotification()}>Criar notificação</button>
-            <button type="button" className="btn btn-info">Ignorar</button>
+  render() {
+    const notification = this.props.notification;
+
+    return (
+      <tr className="notification">
+        <td>{notification.id}</td>
+        <td>{notification.name}</td>
+        <td>{this.formatDate(notification.created_at)}</td>
+        <td>{notification.source_id}</td>
+        <td>{notification.type}</td>
+        <td>{notification.description}</td>
+        <td>
+          <div className="btn-group pull-right">
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={() => this.createNotification()}
+            >
+              Criar notificação
+            </button>
+            <button type="button" className="btn btn-info">
+              Ignorar
+            </button>
           </div>
-				</td>
-			</tr>
-		)
-	}
+        </td>
+      </tr>
+    );
+  }
 }
 
 export default Notification;
--- src/javascripts/app/containers/notifications.js
+++ src/javascripts/app/containers/notifications.js
@@ -1,88 +1,97 @@
-import React from 'react';
-import Notification from '../components/notification';
-import SearchBox from '../components/searchBox';
-import Pagination from '../components/pagination';
-import Alert from '../components/alert';
+import React from "react";
+import Notification from "../components/notification";
+import SearchBox from "../components/searchBox";
+import Pagination from "../components/pagination";
+import Alert from "../components/alert";
 
 class Notifications extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			notifications: [],
-			modal: false,
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: [],
+      modal: false
+    };
 
-		// This binding is necessary to make `this` work in the callback
-		this.loadNotifications = this.loadNotifications.bind(this);
-		this.toggleModal = this.toggleModal.bind(this);
-	}
+    // This binding is necessary to make `this` work in the callback
+    this.loadNotifications = this.loadNotifications.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+  }
 
-	loadNotifications() {
-		fetch('http://localhost:3030/notifications').then((response) => {
-			return response.json();
-		}).then((response) => {
-			// set state
-			this.setState({notifications: response});
-		});
-	}
+  loadNotifications() {
+    fetch(
+      "https://dtupa.eokoe.com/sensor?api_key=f17a9b9d-221a-47c0-9628-07b3a0fd1a59"
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        // set state
+        this.setState({ notifications: response.results });
+      });
+  }
 
-	toggleModal(e) {
-		this.setState({modal: e});
-	}
+  toggleModal(e) {
+    this.setState({ modal: e });
+  }
 
-	componentDidMount() {
-		this.loadNotifications();
-	}
+  componentDidMount() {
+    this.loadNotifications();
+  }
 
-	render() {
-		const hasNotifications = this.state.notifications.length < 1;
-		return (
-			<div className="col-xs-12">
-				<div className="box">
-					<div className="box-header">
-						<h3 className="box-title">Notificações</h3>
+  render() {
+    const hasNotifications = this.state.notifications.length < 1;
+    return (
+      <div className="col-xs-12">
+        <div className="box">
+          <div className="box-header">
+            <h3 className="box-title">Notificações</h3>
 
-						<div className="box-tools">
-							<SearchBox></SearchBox>
-						</div>
-					</div>
-					<div className="box-body table-responsive no-padding">
-						<table className="table table-hover">
-							<tbody><tr>
-								<th>ID</th>
-								<th>Title</th>
-								<th>Date</th>
-								<th>Status</th>
-								<th>Descrição</th>
-								<th></th>
-							</tr>
-							{hasNotifications ? (
-				        <tr>
-									<td colSpan="6">
-										<div className="callout callout-info">
-			                <p>Nenhuma notificação localizada</p>
-			              </div>
-									</td>
-								</tr>
-				      ) : (
-				        this.state.notifications.map((notification) => {
-									return <Notification
-										key={notification.id}
-										notification={notification}
-										toggleModal={this.toggleModal}>
-										</Notification>
-								})
-				      )}
-						</tbody></table>
-					</div>
-					<div className="box-footer clearfix">
-						<Pagination></Pagination>
-					</div>
-				</div>
-				<Alert isOpen={this.state.modal} toggleModal={this.toggleModal}></Alert>
-			</div>
-		)
-	}
+            <div className="box-tools">
+              <SearchBox />
+            </div>
+          </div>
+          <div className="box-body table-responsive no-padding">
+            <table className="table table-hover">
+              <tbody>
+                <tr>
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th>Date</th>
+                  <th>Source</th>
+                  <th>Type</th>
+                  <th>Descrição</th>
+                  <th />
+                </tr>
+                {hasNotifications ? (
+                  <tr>
+                    <td colSpan="6">
+                      <div className="callout callout-info">
+                        <p>Nenhuma notificação localizada</p>
+                      </div>
+                    </td>
+                  </tr>
+                ) : (
+                  this.state.notifications.map(notification => {
+                    return (
+                      <Notification
+                        key={notification.id}
+                        notification={notification}
+                        toggleModal={this.toggleModal}
+                      />
+                    );
+                  })
+                )}
+              </tbody>
+            </table>
+          </div>
+          <div className="box-footer clearfix">
+            <Pagination />
+          </div>
+        </div>
+        <Alert isOpen={this.state.modal} toggleModal={this.toggleModal} />
+      </div>
+    );
+  }
 }
 
 export default Notifications;
