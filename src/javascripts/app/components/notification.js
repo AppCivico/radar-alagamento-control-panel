47180ff29d329ded3378a153275236ecd636333e
Laís Tomaz <lais@eokoe.com>
2017-11-24 19:16:57 -0200

    Populate data into notification component

--- src/javascripts/app/components/notification.js
+++ src/javascripts/app/components/notification.js
@@ -1,31 +1,62 @@
-import React from 'react';
+import React from "react";
+
+import { formatDate } from "../../utils";
 
 class Notification extends React.Component {
-	render() {
-		return (
-			<div className={`modal fade in ${this.props.isOpen ? 'open' : ''}`} id="alert">
-				<div className="modal-dialog">
-					<div className="modal-content">
-						<div className="modal-header">
-							<button type="button" className="close" aria-label="Cancelar" onClick={() => this.props.toggleModal(false)}>
-								<span aria-hidden="true">×</span>
-							</button>
-							<h4 className="modal-title">Criar notificação</h4>
-						</div>
-						<div className="modal-body">
-							<div className="form-group">
-								<label>wefwef</label>
-							</div>
-						</div>
-						<div className="modal-footer">
-							<button type="button" className="btn btn-default pull-left" onClick={() => this.props.toggleModal(false)}>Cancelar</button>
-							<button type="button" className="btn btn-primary">Enviar</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  render() {
+		const alert = this.props.alert;
+
+    return (
+      <div
+        className={`modal fade in ${this.props.isOpen ? "open" : ""}`}
+        id="alert"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                type="button"
+                className="close"
+                aria-label="Cancelar"
+                onClick={() => this.props.toggleModal(false)}
+              >
+                <span aria-hidden="true">×</span>
+              </button>
+              <h4 className="modal-title">Criar notificação</h4>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                {alert.id}
+                <br />
+                {alert.name}
+                <br />
+                {formatDate(alert.created_at)}
+                <br />
+                {alert.source_id}
+                <br />
+                {alert.type}
+                <br />
+                {alert.description}
+                <br />
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-default pull-left"
+                onClick={() => this.props.toggleModal(false)}
+              >
+                Cancelar
+              </button>
+              <button type="button" className="btn btn-primary">
+                Enviar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Notification;
