182e0d1df1b71bfd962df003cb2620904e0cf89e
Laís Tomaz <lais@eokoe.com>
2017-11-24 19:15:05 -0200

    Remove formatDate to prefer utils file methods

--- src/javascripts/app/components/alert.js
+++ src/javascripts/app/components/alert.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { formatDate } from '../../utils';
+
 class Alert extends React.Component {
   constructor() {
     super();
@@ -7,16 +9,8 @@ class Alert extends React.Component {
   }
 
   createAlert() {
+    this.props.changeSelectedAlert(this.props.alert);
     this.props.toggleModal(true);
-  }
-
-  formatDate(date) {
-    if (date)
-      return date
-        .split(" ")[0]
-        .split("-")
-        .reverse()
-        .join("/");
   }
 
   render() {
@@ -26,7 +20,7 @@ class Alert extends React.Component {
       <tr className="alert">
         <td>{alert.id}</td>
         <td>{alert.name}</td>
-        <td>{this.formatDate(alert.created_at)}</td>
+        <td>{formatDate(alert.created_at)}</td>
         <td>{alert.source_id}</td>
         <td>{alert.type}</td>
         <td>{alert.description}</td>
