f9a8242bc12816987298d0b1f11993fbaa6aeba5
Laís Tomaz <laisotomaz@gmail.com>
2017-11-27 20:09:19 -0200

    Add validation method and make lint happy

--- src/javascripts/utils/index.js
+++ src/javascripts/utils/index.js
@@ -1,10 +1,34 @@
-const formatDate = date => {
-  if (date)
-    return date
-      .split(" ")[0]
-      .split("-")
-      .reverse()
-      .join("/");
+const formatDate = (date) => {
+	if (date) {
+		return date
+			.split(' ')[0]
+			.split('-')
+			.reverse()
+			.join('/');
+	}
+
+	return date;
 };
 
-export { formatDate };
+// Pass an object as paramater to validate it's fields
+const validate = (data) => {
+	const validation = {
+		status: false,
+		errors: {},
+	};
+
+	// eslint-disable-next-line array-callback-return
+	Object.keys(data).map((item) => {
+		if (data[item] === '') {
+			validation.errors[item] = 'Campo obrigatório';
+		}
+	});
+
+	if (Object.keys(validation.errors).length < 1) {
+		validation.status = true;
+	}
+
+	return validation;
+};
+
+export { formatDate, validate };
