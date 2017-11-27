0e3e00b14d15f2a4f390a9c3ab74b24191929841
Laís Tomaz <lais@eokoe.com>
2017-11-27 12:46:23 -0200

    Turn notifications into a React component

--- src/javascripts/app/components/notification.js
+++ src/javascripts/app/components/notification.js
@@ -3,56 +3,69 @@ import PropTypes from 'prop-types';
 
 import { formatDate } from '../../utils';
 
-const Notification = (props) => {
-	const alert = props.alert;
-	return (
-		<div className={`modal fade in ${props.isOpen ? 'open' : ''}`} id="alert">
-			<div className="modal-dialog">
-				<div className="modal-content">
-					<div className="modal-header">
-						<button
-							type="button"
-							className="close"
-							aria-label="Cancelar"
-							onClick={() => props.toggleModal(false)}
-						>
-							<span aria-hidden="true">×</span>
-						</button>
-						<h4 className="modal-title">Criar notificação</h4>
-					</div>
-					<div className="modal-body">
-						<div className="form-group">
-							{alert.id}
-							<br />
-							{alert.name}
-							<br />
-							{formatDate(alert.created_at)}
-							<br />
-							{alert.source}
-							<br />
-							{alert.type}
-							<br />
-							{alert.description}
-							<br />
+class Notification extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			notification: {},
+		};
+	}
+
+	sendNotification() {
+		console.log(this.state.notification);
+	}
+
+	render() {
+		const { alert } = this.props;
+		return (
+			<div className={`modal fade in ${this.props.isOpen ? 'open' : ''}`} id="alert">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<button
+								type="button"
+								className="close"
+								aria-label="Cancelar"
+								onClick={() => this.props.toggleModal(false)}
+							>
+								<span aria-hidden="true">×</span>
+							</button>
+							<h4 className="modal-title">Criar notificação</h4>
 						</div>
-					</div>
-					<div className="modal-footer">
-						<button
-							type="button"
-							className="btn btn-default pull-left"
-							onClick={() => props.toggleModal(false)}
-						>
-							Cancelar
-						</button>
-						<button type="button" className="btn btn-primary">
-							Enviar
-						</button>
+						<div className="modal-body">
+							<div className="form-group">
+								{alert.id}
+								<br />
+								{alert.name}
+								<br />
+								{formatDate(alert.created_at)}
+								<br />
+								<br />
+								{alert.type}
+								<br />
+								{alert.description}
+								<br />
+							</div>
+						</div>
+						<div className="modal-footer">
+							<button
+								type="button"
+								className="btn btn-default pull-left"
+								onClick={() => this.props.toggleModal(false)}
+							>
+								Cancelar
+							</button>
+							<button type="button" className="btn btn-primary">
+								Enviar
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 Notification.propTypes = {
 	isOpen: PropTypes.bool.isRequired,
@@ -60,10 +73,7 @@ Notification.propTypes = {
 	alert: PropTypes.objectOf(PropTypes.oneOfType([
 		PropTypes.string,
 		PropTypes.number,
-		PropTypes.objectOf(PropTypes.oneOfType([
-			PropTypes.string,
-			PropTypes.number,
-		])),
+		PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number])),
 	])).isRequired,
 };
 
