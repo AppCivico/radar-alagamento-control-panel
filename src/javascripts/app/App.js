8226af06fd5e973703a13cba12c1a43320f997de
Laís Tomaz <lais@eokoe.com>
2018-02-06 16:42:34 -0200

    Update App component router with Login

--- src/javascripts/app/App.js
+++ src/javascripts/app/App.js
@@ -4,26 +4,56 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './containers/header';
 import Menu from './containers/menu';
 import Alerts from './containers/alerts';
+import Login from './containers/login';
 import NotFound from './containers/notFound';
 
-const App = () => (
-	<Router>
-		<div className="wrapper">
-			<Header />
-			<Menu />
-			<div className="content-wrapper">
-				<div className="content">
-					<div className="row">
-						<Switch>
-							<Route path="/" component={Alerts} />
-							<Route path="/alerts" component={Alerts} />
-							<Route component={NotFound} />
-						</Switch>
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			logged: false,
+		};
+	}
+
+	componentWillMount() {
+		this.getCredentials();
+	}
+
+	getCredentials() {
+		const apiKey = sessionStorage.getItem('apiKey');
+
+		// check if api-key exists, if negative, send user to login page
+		if (apiKey) {
+			this.setState({ logged: true });
+		}
+	}
+
+	render() {
+		return (
+			<Router>
+				<div className="wrapper">
+					{this.state.logged &&
+						<Header />
+					}
+					{this.state.logged &&
+						<Menu />
+					}
+					<div className={`content-wrapper ${!this.state.logged ? 'login' : ''}`}>
+						<div className="content">
+							<div className="row">
+								<Switch>
+									<Route exact path="/" component={Login} />
+									<Route path="/alerts" component={Alerts} />
+									<Route component={NotFound} />
+								</Switch>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</Router>
-);
+			</Router>
+		);
+	}
+}
 
 export default App;
