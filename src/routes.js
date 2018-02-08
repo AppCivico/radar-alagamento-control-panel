546589b3ca684c644571d5ebc90e25ecc64fed7d
Laís Tomaz <lais@eokoe.com>
2018-02-08 17:51:44 -0200

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ import LoginView from './components/Login.vue'
 import NotFoundView from './components/404.vue'
 
 // Import Views - Dash
-import Alerts from './components/views/Alerts.vue'
+import Sensors from './components/views/Sensors.vue'
+import Sensor from './components/views/Sensor.vue'
 import NotificationsSent from './components/views/NotificationsSent.vue'
+import AllNotificationsSent from './components/views/AllNotificationsSent.vue'
 
 // Routes
 const routes = [
@@ -19,12 +21,22 @@ const routes = [
     children: [
       {
         path: '',
-        component: Alerts,
-        name: 'Alertas'
+        component: Sensors,
+        name: 'Sensores'
+      },
+      {
+        path: 'sensor/:id',
+        component: Sensor,
+        name: 'Dados do sensor'
       },
       {
         path: 'notifications-sent',
         component: NotificationsSent,
+        name: 'Notificações enviadas por mim'
+      },
+      {
+        path: 'all-notifications-sent',
+        component: AllNotificationsSent,
         name: 'Notificações enviadas'
       }
     ]
